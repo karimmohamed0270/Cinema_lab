1db0abaf2e0739dd5803f0577aa45545ac20cd31
Karim Mohamed <karimmohamed0270@gmail.com>
2026-04-09 14:38:56 +0200

    provide cubit in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:cinema_lab/get_film_cubit/get_film_cubit.dart';
 import 'package:cinema_lab/views/home_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,6 +13,9 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: HomeScreen(), debugShowCheckedModeBanner: false);
+    return BlocProvider(
+      create: (context) => GetFilmCubit(),
+      child: MaterialApp(home: HomeScreen(), debugShowCheckedModeBanner: false),
+    );
   }
 }
